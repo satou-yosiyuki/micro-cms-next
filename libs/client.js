538266dc6258bc0cb05bbc7aacdfc5b58861e516
satou-yosiyuki <stuysyk1027@icloud.com>
2022-10-18 19:03:22 +0900

    APIKEY設定

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "satou",
-  apiKey:"e60a205ae8164f0aa3d7ab8889ced7ba3304"
+  apiKey: process.env.API_KEY,
 });
