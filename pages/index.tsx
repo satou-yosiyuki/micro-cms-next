bbcf40df166a375f77ec51b5e0169f0f65393562
satou-yosiyuki <stuysyk1027@icloud.com>
2022-10-24 10:24:56 +0900

    記事を横並びにする

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,30 @@ import Link from "next/link";
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { Header } from "../components/Header";
 import { client } from "../libs/client";
+import { Grid } from "@mui/material";
 
 export default function Home({
   blog,
 }): DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   return (
-    <div>
+    <>
       <Header />
-      <ul style={{ listStyle: "none" }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
         {blog.map((blog) => (
-          <li key={blog.id}>
+          <Grid item xs={2} sm={4} md={4} key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
               <a style={{ fontWeight: "bold", fontSize: "20px" }}>
                 {blog.title}
               </a>
             </Link>
-          </li>
+          </Grid>
         ))}
-      </ul>
-    </div>
+      </Grid>
+    </>
   );
 }
 
