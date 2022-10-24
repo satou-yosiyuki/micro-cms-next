334a1258e86d9e9e144b2f43090970d940add887
satou-yosiyuki <stuysyk1027@icloud.com>
2022-10-24 23:52:50 +0900

    環境変数の取得

--- libs/client.ts
+++ libs/client.ts
@@ -1,8 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 require("dotenv").config();
-const { API_KEY } = process.env;
 
 export const client = createClient({
   serviceDomain: "satou",
-  apiKey: "e60a205ae8164f0aa3d7ab8889ced7ba3304",
+  apiKey: process.env.API_KEY,
 });
