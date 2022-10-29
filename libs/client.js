2d2be93ce891dd303138950b6747706733ac4f9c
satou-yosiyuki <stuysyk1027@icloud.com>
2022-10-29 21:26:56 +0900

    環境変数変更

--- libs/client.js
+++ libs/client.js
@@ -1,6 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 require("dotenv").config();
 const env = process.env;
+console.log("env.API_KEY", env.API_KEY);
 
 export const client = createClient({
   serviceDomain: "satou",
